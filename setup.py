6c79f818caf21bab06c3b81dce9a18008b04ffb8
Sammy Pfeiffer <sammypfeiffer@gmail.com>
2017-11-08 17:14:03 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+d = generate_distutils_setup(
+    packages=['htc_vive_helper'],
+    package_dir={'': 'src'}
+)
+
+setup(**d)
